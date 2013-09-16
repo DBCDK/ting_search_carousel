8b82385054e4f65438837d4d7b2a742ad9102215
JrgenGNielsen <jgn@dbc.dk>
2013-09-16 17:28:46 +0200

    create hook for query altering

---
+++ ting_search_carousel.api.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by the ting_search_carousel module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Get fieldsets from other modules.
+ *
+ * @param array $query
+ * @return array array
+ */
+function hook_ting_search_carousel_query(&$query) {
+
+  $query['query'] = $query['query'] . $query['foo']
+
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
+
+
+
+
+
+
+
+
+
+
