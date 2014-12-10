5654cc2f27854698f638f8689d8021a80b94d3ff
Martin Møller <mmj@dbc.dk>
2014-12-10 13:10:55 +0100

    codeformatting

--- templates/ting_search_carousel.tpl.php
+++ templates/ting_search_carousel.tpl.php
@@ -19,18 +19,20 @@
     </div>
 
     <?php if (count($searches) > 1): ?>
-    <div class="rs-carousel-header-select">
-      <form class="bibdk-search-controls-form" data-control-name="controls_carousel">
-        <a class="works-control dropdown-toggle" href="#">
-          <span class="selected-text" tabindex="" accesskey=""><?php print $searches[0]['title'] ?></span>
-        </a>
-        <ul class="dropdown-menu dropdown-rightalign rs-carousel-tabs hidden">
-        <?php foreach ($searches as $i => $search): ?>
-          <li><a class="foo<?php print $i; ?>" href="#<?php print $i; ?>" data-value="<?php print $i; ?>"><?php print $searches[$i]['title'] ?></a></li>
-        <?php endforeach; ?>
-        </ul>
-      </form>
-    </div>
+      <div class="rs-carousel-header-select">
+        <form class="bibdk-search-controls-form" data-control-name="controls_carousel">
+          <a class="works-control dropdown-toggle" href="#">
+            <span class="selected-text" tabindex="" accesskey=""><?php print $searches[0]['title'] ?></span>
+          </a>
+          <ul class="dropdown-menu dropdown-rightalign rs-carousel-tabs hidden">
+            <?php foreach ($searches as $i => $search): ?>
+              <li>
+                <a class="foo<?php print $i; ?>" href="#<?php print $i; ?>" data-value="<?php print $i; ?>"><?php print $searches[$i]['title'] ?></a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        </form>
+      </div>
     <?php endif; ?>
 
   </div>
